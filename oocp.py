347f2427950422b6a7cd483cc1d1564751e71e82
Scott Watson <scott@disney.com>
2013-06-27 08:09:06 -0700

    oocp working in emulation

--- oocp.py
+++ oocp.py
@@ -2,12 +2,13 @@
 
 
 import CCore
-import RPi.GPIO as GPIO  
+# import RPi.GPIO as GPIO  
+import FakeRPiGPIO as GPIO
 import time  
 
 # outputs: 1
 # inputs 
-keyList = [
+scanList = [
 #    [ 1, 1,"A"],
 #    [ 1, 1,"D"],
     [ 1, 3,"Down"],
@@ -41,19 +42,12 @@ keyList = [
     [12,13,"2"]
     ]
 
+# current state of each key
+prev = [0]*len(scanList)
+
 # map keyboard pins to GPIO pins {keyboard:GPIO, ...}
-def pinMap = {1:11}
-
-def sendKey(keyname, state):
-    # send the osc message [keyname, state], where 1 is PRESSED 
-    osc.send("/oocp", [keyname, state])
-
-def test(keymap):
-    outPin = pinMap[keymap[0]]
-    inPin  = pinMap[keymap[1]]
-    GPIO.setup(outPin, GPIO.OUT)
-    GPIO.setup(inPin, GPIO.IN)
-    print GPIO.input(inPin)
+pinMap = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 
+          9:9, 10:10, 11:11, 12:12, 13:13, 14:14, 15:15, 16:16, 17:17}
 
 # initialization 
 osc = CCore.CCore(pubsub="osc-udp:") # use default bidirectional multicast
@@ -61,15 +55,36 @@ osc = CCore.CCore(pubsub="osc-udp:") # use default bidirectional multicast
 # to use Raspberry Pi board pin numbers  
 GPIO.setmode(GPIO.BOARD)  
 
-# set up GPIO output channel  
-GPIO.setup(11, GPIO.OUT)  
+def sendKey(keyname, state):
+    # send the osc message [keyname, state], where 1 is PRESSED 
+    if state:
+        val = 1
+    else:
+        val = 0
+    osc.send("/oocp", [keyname, val])
 
-GPIO.cleanup()   
+def test(keymap):
+    # return the current state of a pin pair
+    outPin = pinMap[keymap[0]]
+    inPin  = pinMap[keymap[1]]
+    GPIO.setup(outPin, GPIO.OUT)
+    GPIO.setup(inPin, GPIO.IN)
+    cur = GPIO.input(inPin)
+    print "pin %s = %s" % (inPin, cur)
+    return cur
 
-# blinking function  
-def blink(pin):  
-        GPIO.output(pin,GPIO.HIGH)  
-        time.sleep(1)  
-        GPIO.output(pin,GPIO.LOW)  
-        time.sleep(1)  
-        return  
+def debounce(index, cur, k):
+    # place to insert debouncing, currently, result is instantaneous
+    if(prev[index]!=cur):
+        prev[index]=cur
+        sendKey(k[2], cur)
+
+def scan(scanList):
+    # itterate through the list 
+    index = 0
+    for k in scanList:
+        cur = test(k)
+        debounce(index,cur,k)
+        index += 1
+
+scan(scanList)
